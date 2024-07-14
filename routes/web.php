dbc5dd203d24c9c9f8a44f112e6e8ca15889cb1d
faizan619 <alamf6023@gmail.com>
2024-07-14 11:12:09 +0530

    routing part done

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,31 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/details',function(){
     return view('home.detailpage');
 });
 
-Route::get('/login',function(){
-    return view('page.login');
-})->name('login');
+// Route::get('/login',function(){
+//     return view('page.login');
+// })->name('login');
 
-Route::get('/register',function(){
-    return view('page.register');
-})->name('register');
+// Route::get('/register',function(){
+//     return view('page.register');
+// })->name('register');
 
 
-Route::resource('user',UserController::class);
+Route::get('login',[LoginController::class,'showLogin'])->name('login');
+Route::post('loginuser',[LoginController::class,'loginUser'])->name('loginUser');
 
-Route::get('/loginuser',[UserController::class,'loginuser'])->name('loginuser');
+Route::delete('logoutuser',[LoginController::class,'logout'])->name('logout');
+
+Route::get('register',[RegisterController::class,'showRegister'])->name('register');
+Route::post('registeruser',[RegisterController::class,'registerUser'])->name('registerUser');
+
